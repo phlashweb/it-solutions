b871df20846334d7d15da359c0e25c0a15f01f42
Ben Cooper <ben.f.cooper@gmail.com>
2024-08-05 09:46:30 +0100

    include ip limiter

--- main.go
+++ main.go
@@ -1,41 +1,65 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
-	"encoding/json"
 	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+	"slices"
+
+	"github.com/joho/godotenv"
 )
 
 type FormData struct {
-	Name              string `json:"name"`
-	Email             string `json:"email"`
-	PhoneNumber       string `json:"phone_number"`
-	Subject           string `json:"subject"`
-	Message           string `json:"message"`
-	ServiceSelection  string `json:"service_selection"`
-	PrivacyPolicy     string `json:"privacy_policy"`
+	Name             string `json:"name"`
+	Email            string `json:"email"`
+	PhoneNumber      string `json:"phone_number"`
+	Subject          string `json:"subject"`
+	Message          string `json:"message"`
+	ServiceSelection string `json:"service_selection"`
+	PrivacyPolicy    string `json:"privacy_policy"`
+}
+
+type IPMessages struct {
+	IP           string `json:"ip"`
+	MessageCount int    `json:"message_count"`
 }
 
 func main() {
-    // Serve static files
-    http.Handle("/", http.FileServer(http.Dir(".")))
-    
-    // Define handler for POST request
-    http.HandleFunc("/api/message", func(w http.ResponseWriter, r *http.Request) {
-        if r.Method != "POST" {
+	godotenv.Load()
+
+	var ipData []IPMessages
+
+	// Serve static files
+	http.Handle("/", http.FileServer(http.Dir(".")))
+
+	// Define handler for POST request
+	http.HandleFunc("/api/message", func(w http.ResponseWriter, r *http.Request) {
+		numberOfTimesIPHasBeenSeen := 0
+		for i := 0; i < len(ipData); i++ {
+			if ipData[i].IP == r.RemoteAddr {
+				numberOfTimesIPHasBeenSeen = ipData[i].MessageCount
+				ipData = slices.Delete(ipData, i, i+1)
+				fmt.Println("IP: ", r.RemoteAddr, " Name: ", r.FormValue("your-name"))
+			}
+		}
+
+		ipData = append(ipData, IPMessages{IP: r.RemoteAddr, MessageCount: numberOfTimesIPHasBeenSeen + 1})
+
+		if r.Method != "POST" {
 			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 			return
 		}
 
 		formData := FormData{
-			Name:              r.FormValue("your-name"),
-			Email:             r.FormValue("your-email"),
-			PhoneNumber:       r.FormValue("phone-number"),
-			Subject:           r.FormValue("your-subject"),
-			Message:           r.FormValue("message"),
-			ServiceSelection:  r.FormValue("service-selection"),
-			PrivacyPolicy:     r.FormValue("privacy-policy"),
+			Name:             r.FormValue("your-name"),
+			Email:            r.FormValue("your-email"),
+			PhoneNumber:      r.FormValue("phone-number"),
+			Subject:          r.FormValue("your-subject"),
+			Message:          r.FormValue("message"),
+			ServiceSelection: r.FormValue("service-selection"),
+			PrivacyPolicy:    r.FormValue("privacy-policy"),
 		}
 
 		// Convert struct to JSON
@@ -59,10 +83,13 @@ func main() {
 
 		// Print JSON data
 		fmt.Println("JSON Data:", string(jsonData))
-	
-		
+
 		// Define the URL to send the POST request to
 		url := ""
+		discordWebhookURL, exists := os.LookupEnv("DISCORD_WEBHOOK_URL")
+		if exists {
+			url = discordWebhookURL
+		}
 
 		// Create the JSON content
 		contentSend := map[string]string{"content": string(jsonData)}
@@ -85,13 +112,15 @@ func main() {
 		// Create a new HTTP client
 		client := &http.Client{}
 
-		// Send the request
-		resp, err := client.Do(req)
-		if err != nil {
-			fmt.Println("Error sending request:", err)
-			return
+		if numberOfTimesIPHasBeenSeen < 2 || r.FormValue("your-name") == "Marie" {
+			// Send the request
+			resp, err := client.Do(req)
+			if err != nil {
+				fmt.Println("Error sending request:", err)
+				return
+			}
+			defer resp.Body.Close()
 		}
-		defer resp.Body.Close()
 	})
 
 	// Development
